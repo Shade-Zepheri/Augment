7002cc76d76ee40af7b53585a431dce525eeed32
Shade-Zepheri <ziroalpha@gmail.com>
2016-10-12 19:49:28 -0500

    This might be my prettiest tweak yet, added header

--- augment/AUGRootListController.h
+++ augment/AUGRootListController.h
@@ -1,4 +1,7 @@
 #import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSViewController.h>
+
 
 @interface AUGRootListController : PSListController
 
--- augment/AUGRootListController.m
+++ augment/AUGRootListController.m
@@ -4,10 +4,38 @@
 
 - (NSArray *)specifiers {
 	if (!_specifiers) {
-		_specifiers = [[self loadSpecifiersFromPlistName:@"Augment" target:self] retain];
+		_specifiers = [self loadSpecifiersFromPlistName:@"Augment" target:self];
 	}
 
 	return _specifiers;
+}
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+
+	CGRect frame = CGRectMake(0, 0, self.table.bounds.size.width, 127);
+
+	UIImage *headerImage = [[UIImage alloc]
+		initWithContentsOfFile:[[NSBundle bundleWithPath:@"/Library/PreferenceBundles/augment.bundle"] pathForResource:@"AugmentHeader" ofType:@"png"]];
+
+	UIImageView *headerView = [[UIImageView alloc] initWithFrame:frame];
+	[headerView setImage:headerImage];
+	headerView.backgroundColor = [UIColor blackColor];
+	[headerView setContentMode:UIViewContentModeScaleAspectFit];
+	[headerView setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
+
+	self.table.tableHeaderView = headerView;
+}
+
+- (void)viewDidLayoutSubviews
+{
+	[super viewDidLayoutSubviews];
+
+	CGRect wrapperFrame = ((UIView *)self.table.subviews[0]).frame; // UITableViewWrapperView
+	CGRect frame = CGRectMake(wrapperFrame.origin.x, self.table.tableHeaderView.frame.origin.y, wrapperFrame.size.width, self.table.tableHeaderView.frame.size.height);
+
+	self.table.tableHeaderView.frame = frame;
 }
 
 @end
